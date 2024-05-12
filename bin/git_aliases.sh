229198f9af8f9da610533ad72fdcf6e0a83eae8a
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-05-12 22:14:39 +1000

    add file

---
+++ bin/git_aliases.sh
@@ -0,0 +1,13 @@
+alias gs='git status'
+alias gd='git diff'
+alias gc="git commit -m '.'"
+alias gm='git merge'
+alias gph='git push'
+alias gpl='git pull'
+alias gca='git add . && git commit --amend'
+alias dotcommit="git add . && git commit -m '.'"
+
+alias gfb='git flow bugfix start'
+alias gfh='git flow hotfix start'
+alias gfr='git flow release start'
+alias gff='git flow feature start'
