9ad1771c2826b682ce54bc686a058df59b618d1c
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-05-12 22:47:26 +1000

    Improve docker tag alias and add references

--- bin/docker_aliases.sh
+++ bin/docker_aliases.sh
@@ -1,3 +1,8 @@
 alias dpl='docker pull'
 alias dph='docker push'
-alias dt='docker tag'
+alias dt='docker images | fzf | awk '{print $1":"$2}' # Not yet operational
+
+# ---
+# References 
+#
+# [1] https://www.youtube.com/watch?v=IYZDIhfAUM0
