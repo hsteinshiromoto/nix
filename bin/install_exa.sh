1d9f84044f6bb151e4b3f56feb229774ca86bf09
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-06-05 21:53:21 +1000

    Remove sudo command

--- bin/install_exa.sh
+++ bin/install_exa.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo mkdir -p /etc/apt/keyrings
-wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-sudo apt update
-sudo apt install -y eza
+mkdir -p /etc/apt/keyrings
+wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list
+chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+apt update
+apt install -y eza
 
 # References:
 #   [1] https://github.com/eza-community/eza/blob/main/INSTALL.md
