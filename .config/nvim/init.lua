81baa2c67c888b10404a1b2f17bc34478c10f0b4
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-06-06 21:38:21 +1000

    Add barbar plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -48,7 +48,19 @@ local plugins = {
 	{ "mbbill/undotree" },
 	{ 'jiangmiao/auto-pairs' },
 	{ "HiPhish/rainbow-delimiters.nvim" },
-	{ "tpope/vim-commentary" }
+	{ "tpope/vim-commentary" },
+	{'romgrk/barbar.nvim',
+		dependencies = {
+			'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+			'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+		},
+		init = function() vim.g.barbar_auto_setup = false end,
+		opts = {-- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+			-- animation = true,
+			-- insert_at_start = true,
+			},
+    		version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  	},	
 }
 local opts = {}
 
