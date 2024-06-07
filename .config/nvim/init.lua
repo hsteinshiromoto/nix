5393d1be756e9a32f84ab6f31678733f8f63b60a
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-06-07 22:04:00 +1000

    Add tmux-navigator
    Add to neovim and tmux plugins.
    Integrate tmux-navigator into neovim and tmux config files.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -60,7 +60,22 @@ local plugins = {
 			-- insert_at_start = true,
 			},
     		version = '^1.0.0', -- optional: only update when a new 1.x version is released
-  	},	
+  	},
+	{ 'alexghergh/nvim-tmux-navigation', config = function()
+		
+		local nvim_tmux_nav = require('nvim-tmux-navigation')
+		nvim_tmux_nav.setup {
+			disable_when_zoomed = true -- defaults to false
+		}
+
+		vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+		vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+		vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+		vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+		vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+
+		end
+	}
 }
 local opts = {}
 
