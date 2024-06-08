ba040f957fc832ba625af6c74b44e22e7ffba231
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-06-08 22:24:50 +1000

    Move treesitter nvim plugin to its own config file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,22 +13,6 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
 
--- Setup Treesitter
--- In Linux, TreeSitter requires rust and tree-sitter-cli to be installed [1]. To do this:
--- 	1. Install Rust using the instructions from [2].
--- 	2. Configure cargo to be used with zsh with the command `. "$HOME/.cargo/env"`. Please note the leading dot in this command.
--- 	3. Install treesitter-cli with the command `cargo install tree-sitter-cli`
--- References:
--- 	[1] https://github.com/nvim-treesitter/nvim-treesitter/issues/1097#issuecomment-1368177624
--- 	[2] https://www.rust-lang.org/tools/install
-local config = require("nvim-treesitter.configs")
-config.setup({
-	ensure_installed = {"dockerfile", "latex", "lua", "make", "python", "toml", "vim", "vimdoc"},
-	auto_install = true,
-	highlight = {enable = true },
-	indent = { enable = true}
-	})
-
 -- Setup relative and absolute numbers
 -- References:
 --	[1] https://www.reddit.com/r/neovim/comments/14xxw1p/display_both_relative_and_absolute_line_numbers/
---
+++ .config/nvim/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+-- Setup Treesitter
+-- In Linux, TreeSitter requires rust and tree-sitter-cli to be installed [1]. To do this:
+-- 	1. Install Rust using the instructions from [2].
+-- 	2. Configure cargo to be used with zsh with the command `. "$HOME/.cargo/env"`. Please note the leading dot in this command.
+-- 	3. Install treesitter-cli with the command `cargo install tree-sitter-cli`
+-- References:
+-- 	[1] https://github.com/nvim-treesitter/nvim-treesitter/issues/1097#issuecomment-1368177624
+-- 	[2] https://www.rust-lang.org/tools/install
+return { "nvim-treesitter/nvim-treesitter"
+	,build = ":TSUpdate"
+	,config = function()
+		local config = require("nvim-treesitter.configs")
+		config.setup({	auto_install = true
+			,highlight = { enable = true }
+			,indent = { enable = true }
+			,ensure_installed = {"dockerfile", "latex", "lua", "make", "python", "toml", "vim", "vimdoc"}
+		})
+	end
+}
+
