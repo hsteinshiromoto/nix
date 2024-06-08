893c3591df21e3693dd7009fcf9cfc7870e25654
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-06-08 22:44:06 +1000

    Move nvim vimtex plugin to its own file

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -1,11 +1,5 @@
 return {
-	{ "sitiom/nvim-numbertoggle" },
-	{ "lervag/vimtex",
-  		--init = function()
-    		-- VimTeX configuration goes here, e.g.
-    		-- vim.g.vimtex_view_method = "zathura"
-  		--end
-	},
+	{ "sitiom/nvim-numbertoggle" },	
 	{"NeogitOrg/neogit",
   		dependencies = {"nvim-lua/plenary.nvim",         -- required
     				"sindrets/diffview.nvim",        -- optional - Diff integration
---
+++ .config/nvim/plugins/vimtex.lua
@@ -0,0 +1,6 @@
+return { "lervag/vimtex",
+  		--init = function()
+    		-- VimTeX configuration goes here, e.g.
+    		-- vim.g.vimtex_view_method = "zathura"
+  		--end
+}
