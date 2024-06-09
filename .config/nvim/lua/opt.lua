4c8e3fae641dd451a5b59604b75385efdacd9a74
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-06-09 22:48:54 +1000

    Remap leader and localleader nvim keys

--- .config/nvim/lua/opt.lua
+++ .config/nvim/lua/opt.lua
@@ -2,6 +2,9 @@
 -- References:
 --	[1] https://www.reddit.com/r/neovim/comments/14xxw1p/display_both_relative_and_absolute_line_numbers/
 
+vim.g.mapleader = ";"
+vim.g.maplocalleader = ","
+
 vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.o.statuscolumn = "%s %l %r "
