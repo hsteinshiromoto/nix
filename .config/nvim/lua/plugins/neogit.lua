db61e27b95459cb580f1a75058deb312d998415c
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-06-10 20:35:30 +1000

    Improve formatting of file

--- .config/nvim/lua/plugins/neogit.lua
+++ .config/nvim/lua/plugins/neogit.lua
@@ -1,11 +1,13 @@
-return 	{ "NeogitOrg/neogit"
-		,dependencies = {"nvim-lua/plenary.nvim"         -- required
-    				,"sindrets/diffview.nvim"        -- optional - Diff integration
-    				-- Only one of these is needed, not both.
-    				,"nvim-telescope/telescope.nvim", -- optional
-    				-- "ibhagwan/fzf-lua",              -- optional
-		}
-		,config = function()
-			vim.keymap.set('n', '<C-g>', ":Neogit<CR>")
-		end
-} 
+return {
+	"NeogitOrg/neogit",
+	dependencies = {
+		"nvim-lua/plenary.nvim", -- required
+		"sindrets/diffview.nvim"		-- Only one of these is needed, not both.
+, -- optional - Diff integration
+		"nvim-telescope/telescope.nvim", -- optional
+		-- "ibhagwan/fzf-lua",              -- optional
+	},
+	config = function()
+		vim.keymap.set("n", "<C-g>", ":Neogit<CR>")
+	end,
+}
