e9531008e579c41257907015f3d08223707bc322
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-09-16 09:44:58 +1000

    Update shortcut to find files using telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,23 +1,24 @@
 return {
-	{'nvim-telescope/telescope.nvim',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-		,config = function()
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "fzf", builtin.find_files, {})
+			vim.keymap.set("n", "find", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>f", builtin.live_grep, {})
-		end
+		end,
 	},
-	{ 'nvim-telescope/telescope-ui-select.nvim'
-		,config = function()
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
 			require("telescope").setup({
-  				extensions = { 
+				extensions = {
 					["ui-select"] = {
-      						require("telescope.themes").get_dropdown {  }
-					}
-				}
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
 			})
 			require("telescope").load_extension("ui-select")
-		end
-
-	}
+		end,
+	},
 }
