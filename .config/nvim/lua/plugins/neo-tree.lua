c6a4ba356c80795bf4948c16bc63cabfc6b4edce
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-09-17 17:07:31 +1000

    Reformat

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -1,20 +1,22 @@
-return { "nvim-neo-tree/neo-tree.nvim"
-	,dependencies = { "nvim-lua/plenary.nvim"
-		,"nvim-tree/nvim-web-devicons" -- not strictly required, but recommended
-		,"MunifTanjim/nui.nvim"
-      		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-    			}
-	,config = function()
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+	},
+	config = function()
 		require("neo-tree").setup({
 			filesystem = {
-			    filtered_items = {
-			      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
-			      hide_dotfiles = false,
-			      hide_gitignored = false,
-			    },
-		  	use_libuv_file_watcher=true,
-			}
+				filtered_items = {
+					visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+					hide_dotfiles = false,
+					hide_gitignored = false,
+				},
+				use_libuv_file_watcher = true,
+			},
 		})
 		vim.keymap.set("n", "<leader>tree", ":Neotree filesystem toggle left<CR>")
-	end
+	end,
 }
