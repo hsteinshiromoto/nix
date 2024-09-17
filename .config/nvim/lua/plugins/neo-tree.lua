7e4414c8afb09234dd5ec75361fcfc040f05a888
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-09-17 17:05:54 +1000

    NeoTree Config Change
    Set hiddne variables to be always visible and move the
    use_libuv_file_watcher to the setup dict

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -5,8 +5,16 @@ return { "nvim-neo-tree/neo-tree.nvim"
       		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
     			}
 	,config = function()
-		require("neo-tree").setup({})
+		require("neo-tree").setup({
+			filesystem = {
+			    filtered_items = {
+			      visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+			      hide_dotfiles = false,
+			      hide_gitignored = false,
+			    },
+		  	use_libuv_file_watcher=true,
+			}
+		})
 		vim.keymap.set("n", "<leader>tree", ":Neotree filesystem toggle left<CR>")
-		use_libuv_file_watcher=true
 	end
 }
