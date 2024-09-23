41026dbc8a5cc110c0dcbd0211b95eb6bd8c7161
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-09-23 10:47:48 +1000

    Add description to keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -4,8 +4,8 @@ return {
 		dependencies = { "nvim-lua/plenary.nvim" },
 		config = function()
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-f>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>gr", builtin.live_grep, {})
+			vim.keymap.set("n", "<C-f>", builtin.find_files, { desc = "Find File" } )
+			vim.keymap.set("n", "<leader>gr", builtin.live_grep, { desc = "Live Grep"} )
 		end,
 	},
 	{
