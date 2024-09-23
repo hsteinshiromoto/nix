c9f89d22471fcdbd32765b37eb9d24a874ab0194
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-09-23 10:29:13 +1000

    Add keymaps for code-runner plugin
    Add recommended keymaps for code-runner plugin based on leader key

--- .config/nvim/lua/plugins/code_runner.lua
+++ .config/nvim/lua/plugins/code_runner.lua
@@ -7,6 +7,13 @@ return {
 				python = "python3 -u '$dir/$fileName'",
 				sh = "bash",
 			},
+			vim.keymap.set("n", "<leader>r", ":RunCode<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<leader>rf", ":RunFile<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<leader>rft", ":RunFile tab<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<leader>rp", ":RunProject<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<leader>rc", ":RunClose<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<leader>crf", ":CRFiletype<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<leader>crp", ":CRProjects<CR>", { noremap = true, silent = false }),
 		})
 	end,
 }
