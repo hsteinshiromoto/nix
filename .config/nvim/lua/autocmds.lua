7094b26e486fbc27460a03da2d5d3eb4e2bf692f
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-09-29 10:25:46 +1000

    Changes to variables in autocmds.lua
    Change variable from sussmanGroup to localGroup
    Add references and comments

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,9 +1,14 @@
+---
+--- References:
+--- 	[1] https://www.dmsussman.org/resources/neovimsetup/
+---
+
 local autogroup = vim.api.nvim_create_augroup
-local sussmanGroup = autogroup("DMS", {})
+local localGroup = autogroup("DMS", {})
 local autocmd = vim.api.nvim_create_autocmd
 -- resize splits if the window itself is resized
 autocmd("VimResized", {
-	group = sussmanGroup,
+	group = localGroup,
 	callback = function()
 		local currentTab = vim.fn.tabpagenr()
 		vim.cmd("tabdo wincmd =")
