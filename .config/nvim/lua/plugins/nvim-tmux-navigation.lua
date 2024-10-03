92dfe199c51b54d018868f3755616e838a03c7fe
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-10-04 08:41:08 +1000

    Add previous tmux pane keystroke command

--- .config/nvim/lua/plugins/nvim-tmux-navigation.lua
+++ .config/nvim/lua/plugins/nvim-tmux-navigation.lua
@@ -8,6 +8,10 @@ return {
 			disable_when_zoomed = false, -- defaults to false
 		})
 
+		vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft, {desc = "Tmux Left"})
+		vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, {desc = "Tmux Down"})
+		vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, {desc = "Tmux Up"})
+		vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight, {desc = "Tmux Right"})
 		vim.keymap.set("n", "<C-g>", nvim_tmux_nav.NvimTmuxNavigateLastActive, {desc = "Tmux Previous Panel"})
 		vim.keymap.set("n", "<C-n>", nvim_tmux_nav.NvimTmuxNavigateNext)
 	end,
