a64d86cb2831e5ed3be570f3eb5fc1ad58a8ddbe
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-10-14 19:31:52 +1100

    Add home-manager module to darwin flake

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -6,9 +6,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, home-manager, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
@@ -47,7 +49,18 @@
     # Build darwin flake using:
     # $ darwin-rebuild build --flake .#MacBook-Pro-2023
     darwinConfigurations."MacBook-Pro-2023" = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [ 
+      	configuration 
+	home-manager.darwinModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.hsteinshiromoto = import ./home.nix;
+
+            # Optionally, use home-manager.extraSpecialArgs to pass
+            # arguments to home.nix
+          }
+	];
     };
 
     # Expose the package set, including overlays, for convenience.
