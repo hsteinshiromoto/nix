b3e64097707509f08dd3eca401c5aaf07563fd65
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-10-20 22:34:08 +1100

    Add keystrokes to configure barbar nvim plugin
    Add <leader><Left> (resp. <Right>) to move between previous and next
    tabs

--- .config/nvim/lua/plugins/barbar.lua
+++ .config/nvim/lua/plugins/barbar.lua
@@ -1,11 +1,16 @@
-return { 'romgrk/barbar.nvim'
-	,dependencies = { 'lewis6991/gitsigns.nvim' -- OPTIONAL: for git status
-		,'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
-		}
-	,event = "BufEnter"
-	,config = function()
+return {
+	"romgrk/barbar.nvim",
+	dependencies = {
+		"lewis6991/gitsigns.nvim", -- OPTIONAL: for git status
+		"nvim-tree/nvim-web-devicons", -- OPTIONAL: for file icons
+	},
+	event = "BufEnter",
+	config = function()
 		require("barbar").setup({})
-		vim.keymap.set('n', '<C-t>', ":tabnew<CR>", { desc="Create new tab"} )
+		vim.keymap.set("n", "<C-t>", ":tabnew<CR>", { desc = "Create New Tab" })
+		-- Move to previous/next
+		vim.keymap.set('n', '<leader><Left>', '<Cmd>BufferPrevious<CR>', { desc = "Previous Tab" })
+		vim.keymap.set('n', '<leader><Right>', '<Cmd>BufferNext<CR>', { desc = "Next tab" })
 		vim.g.barbar_auto_setup = false
-	end
+	end,
 }
