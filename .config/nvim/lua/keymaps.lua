1c0ad8c39d5248cbb950a46e027de1d1b3a44dff
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-01 22:17:22 +1100

    Add keymaps file

---
+++ .config/nvim/lua/keymaps.lua
@@ -0,0 +1,27 @@
+local keymap = vim.keymap.set
+
+-- Better escape using jk in insert and terminal mode [1]
+keymap("i", "jk", "<ESC>", {desc = '<ESC>'})
+keymap("t", "jk", "<C-\\><C-n>", {desc = '<ESC>'})
+keymap("t", "<C-h>", "<C-\\><C-n><C-w>h")
+keymap("t", "<C-j>", "<C-\\><C-n><C-w>j")
+keymap("t", "<C-k>", "<C-\\><C-n><C-w>k")
+keymap("t", "<C-l>", "<C-\\><C-n><C-w>l")
+
+-- Better indent [1]
+keymap("v", "<", "<gv", {desc="Indent Left"})
+keymap("v", ">", ">gv", {desc="Indent Right"})
+
+-- Paste over currently selected text without yanking it [1]
+keymap("v", "p", '"_dP', {desc="Paste over currently selected text without yanking it"})
+
+-- Move Lines [1]
+keymap("n", "<A-j>", ":m .+1<CR>==", {desc='Move Lines'})
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", {desc='Move Lines'})
+keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", {desc='Move Lines'})
+keymap("n", "<A-k>", ":m .-2<CR>==", {desc='Move Lines'})
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", {desc='Move Lines'})
+keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", {desc='Move Lines'})
+
+-- References
+-- 	[1] https://alpha2phi.medium.com/modern-neovim-init-lua-ab1220e3ecc1
