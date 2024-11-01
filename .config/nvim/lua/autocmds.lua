d976a7f7346eeb8b55043b4d41fdd4caa9d66f68
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-01 21:59:04 +1100

    Change autocmds.lua
    Reload file when changed
    Go to last location when opening a buffer

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -1,12 +1,9 @@
----
---- References:
---- 	[1] https://www.dmsussman.org/resources/neovimsetup/
----
+-- resize splits if the window itself is resized [2]
 
 local autogroup = vim.api.nvim_create_augroup
 local localGroup = autogroup("DMS", {})
 local autocmd = vim.api.nvim_create_autocmd
--- resize splits if the window itself is resized
+-- resize splits if the window itself is resized [2]
 autocmd("VimResized", {
 	group = localGroup,
 	callback = function()
@@ -16,6 +13,7 @@ autocmd("VimResized", {
 	end,
 })
 
+-- Highlight in Yank [2]
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
 	callback = function()
@@ -24,3 +22,27 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
+
+-- Check if we need to reload the file when it changed [2]
+vim.api.nvim_create_autocmd("FocusGained", { command = "checktime" })
+
+-- Go to last loc when opening a buffer [2]
+vim.api.nvim_create_autocmd("BufReadPre", {
+  pattern = "*",
+  callback = function()
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "<buffer>",
+      once = true,
+      callback = function()
+        vim.cmd(
+          [[if &ft !~# 'commit\|rebase' && line("'\"") > 1 && line("'\"") <= line("$") | exe 'normal! g`"' | endif]]
+        )
+      end,
+    })
+  end,
+})
+
+--- References:
+--- 	[1] https://www.dmsussman.org/resources/neovimsetup/
+---	[2] https://alpha2phi.medium.com/modern-neovim-init-lua-ab1220e3ecc1
+
