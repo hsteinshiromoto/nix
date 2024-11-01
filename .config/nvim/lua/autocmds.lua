59de871f90cf6251a2b346d1fb9527821ec7a7e4
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-01 21:54:11 +1100

    Migrate highligh on yank from opt.lua to autocmds.lua

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -15,3 +15,12 @@ autocmd("VimResized", {
 		vim.cmd("tabnext " .. currentTab)
 	end,
 })
+
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
