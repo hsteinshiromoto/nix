b481f6a3e648cb72ce92e76f1f3e1d7fdea40245
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-02 13:51:21 +1100

    Setup colorscheme

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,10 @@
+local indent = 2
+
+vim.opt.shiftwidth = indent
+vim.opt.tabstop = indent
+vim.opt.ignorecase = true
+
+
 -- Setup relative and absolute numbers
 -- References:
 --	[1] https://www.reddit.com/r/neovim/comments/14xxw1p/display_both_relative_and_absolute_line_numbers/
@@ -10,11 +17,11 @@ vim.opt.relativenumber = true
 vim.o.statuscolumn = "%s %l %r "
 
 -- Setup Neotree to open when NeoVim starts
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		vim.cmd("Neotree")
-	end,
-})
+-- vim.api.nvim_create_autocmd("VimEnter", {
+-- 	callback = function()
+-- 		vim.cmd("Neotree")
+-- 	end,
+-- })
 
 -- Setup NeoVim to yank to clipboard
 vim.opt.clipboard = "unnamedplus"
