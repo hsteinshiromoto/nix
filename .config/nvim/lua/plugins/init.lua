7f5fa2cc9342b2f4ebdb2bd38c6a331b72ebfcd8
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-02 19:52:51 +1100

    Move nvim-tmux-navigation plugin to init.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -34,4 +34,22 @@ return {
 			vim.api.nvim_set_keymap("v", "g<C-x>", require("dial.map").dec_gvisual(), { noremap = true })
 		end,
 	},
+	{
+		"alexghergh/nvim-tmux-navigation",
+		lazy = false,
+		config = function()
+			local nvim_tmux_nav = require("nvim-tmux-navigation")
+
+				nvim_tmux_nav.setup({
+					disable_when_zoomed = false, -- defaults to false
+				})
+
+				vim.keymap.set("n", "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft, { desc = "Tmux Left" })
+				vim.keymap.set("n", "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown, { desc = "Tmux Down" })
+				vim.keymap.set("n", "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp, { desc = "Tmux Up" })
+				vim.keymap.set("n", "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight, { desc = "Tmux Right" })
+				vim.keymap.set("n", "<C-g>", nvim_tmux_nav.NvimTmuxNavigateLastActive, { desc = "Tmux Previous Panel" })
+				vim.keymap.set("n", "<C-n>", nvim_tmux_nav.NvimTmuxNavigateNext)
+			end,
+	},
 }
