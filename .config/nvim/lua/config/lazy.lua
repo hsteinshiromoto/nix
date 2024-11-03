091979df0f7222010c98176f3b16d0da0f539742
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-03 22:07:00 +1100

    Add comment

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -16,7 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	spec = {
 		{ import = "plugins" },
-		-- { import = "plugins.extras.lang" },
+		-- { import = "plugins.extras.lang" }, -- This raises bug. Do not uncomment
 		{ import = "plugins.extras.ui" },
 	},
 	defaults = { lazy = true, version = nil },
