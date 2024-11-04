d16b4acd4430aa6802bb503590a20692fee916bd
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-04 21:13:18 +1100

    Configure template folder in obsidian.nvim

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -35,7 +35,13 @@ return {
 				enable = true,
 			},
 		}),
-
 		vim.opt.conceallevel == 1,
+		templates = {
+			folder = "_meta_/_templates_",
+			date_format = "%Y-%m-%d",
+			time_format = "%H:%M",
+			-- A map for custom variables, the key should be the variable and the value a function
+			substitutions = {},
+		},
 	},
 }
