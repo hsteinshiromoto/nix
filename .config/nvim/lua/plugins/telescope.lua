ee313ba65e205f0dd55ace740997ff7609b9d2ea
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-04 21:52:48 +1100

    Improve telescope nvim plugin add dependency for dap
    Augment telescope nvim plugin functionality.
    Add dependency for nvim dap.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,13 +1,97 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  cmd = "Telescope",
-  keys = {
-    { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-    { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-    { "<leader>fg", "<cmd>Telescope git_files<cr>", desc = "Git Files" },
-    { "<leader>f/", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
-  },
-  config = true,
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+			"nvim-telescope/telescope-file-browser.nvim",
+			"cljoly/telescope-repo.nvim",
+			"stevearc/aerial.nvim",
+			"nvim-telescope/telescope-frecency.nvim",
+		},
+		cmd = "Telescope",
+		keys = {
+			{ "<leader><space>", require("utils").find_files, desc = "Find Files" },
+			{ "<leader>ff", require("utils").find_files, desc = "Find Files" },
+			{ "<leader>fo", "<cmd>Telescope frecency theme=dropdown previewer=false<cr>", desc = "Recent" },
+			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+			{ "<leader>fr", "<cmd>Telescope file_browser<cr>", desc = "Browser" },
+			{ "<leader>ps", "<cmd>Telescope repo list<cr>", desc = "Search" },
+			{ "<leader>hs", "<cmd>Telescope help_tags<cr>", desc = "Search" },
+			{ "<leader>sw", "<cmd>Telescope live_grep<cr>", desc = "Workspace" },
+			{
+				"<leader>sb",
+				function()
+					require("telescope.builtin").current_buffer_fuzzy_find()
+				end,
+				desc = "Buffer",
+			},
+			{ "<leader>vo", "<cmd>Telescope aerial<cr>", desc = "Code Outline" },
+		},
+		config = function(_, _)
+			local telescope = require("telescope")
+			local icons = require("config.icons")
+			local actions = require("telescope.actions")
+			local actions_layout = require("telescope.actions.layout")
+			local mappings = {
+				i = {
+					["<C-j>"] = actions.move_selection_next,
+					["<C-k>"] = actions.move_selection_previous,
+					["<C-n>"] = actions.cycle_history_next,
+					["<C-p>"] = actions.cycle_history_prev,
+					["?"] = actions_layout.toggle_preview,
+				},
+			}
+
+			local opts = {
+				defaults = {
+					prompt_prefix = icons.ui.Telescope .. " ",
+					selection_caret = icons.ui.Forward .. " ",
+					mappings = mappings,
+					border = {},
+					borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+					color_devicons = true,
+				},
+				pickers = {
+					find_files = {
+						theme = "dropdown",
+						previewer = false,
+						hidden = true,
+						find_command = { "rg", "--files", "--hidden", "-g", "!.git" },
+					},
+					git_files = {
+						theme = "dropdown",
+						previewer = false,
+					},
+					buffers = {
+						theme = "dropdown",
+						previewer = false,
+					},
+				},
+				extensions = {
+					file_browser = {
+						theme = "dropdown",
+						previewer = false,
+						hijack_netrw = true,
+						mappings = mappings,
+					},
+					project = {
+						hidden_files = false,
+						theme = "dropdown",
+					},
+				},
+			}
+			telescope.setup(opts)
+			telescope.load_extension("fzf")
+			telescope.load_extension("file_browser")
+			-- telescope.load_extension("project")
+			-- telescope.load_extension("projects")
+			telescope.load_extension("aerial")
+			telescope.load_extension("dap")
+			telescope.load_extension("frecency")
+		end,
+	},
+	{
+		"stevearc/aerial.nvim",
+		config = true,
+	},
 }
