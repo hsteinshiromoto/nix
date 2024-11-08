0aeb1567170dcdb1f1d28cd6cda8f6318d7f772f
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-08 22:04:55 +1100

    Remove unnecessary languages loaded with neogen

--- .config/nvim/lua/plugins/neogen.lua
+++ .config/nvim/lua/plugins/neogen.lua
@@ -14,26 +14,6 @@ return {
 					annotation_convention = "google_docstrings",
 				},
 			},
-			rust = {
-				template = {
-					annotation_convention = "rustdoc",
-				},
-			},
-			javascript = {
-				template = {
-					annotation_convention = "jsdoc",
-				},
-			},
-			typescript = {
-				template = {
-					annotation_convention = "tsdoc",
-				},
-			},
-			typescriptreact = {
-				template = {
-					annotation_convention = "tsdoc",
-				},
-			},
 		},
 	},
   --stylua: ignore
