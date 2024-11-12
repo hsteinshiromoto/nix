c437fdfd5c8f054fd51dbdaa771babb2dc8c34d4
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-13 10:55:52 +1100

    Revert "Delete code runner"
    This reverts commit e029acbc6bfa2471cec6fea2ceca207a1135ca65.

---
+++ .config/nvim/lua/plugins/code_runner.lua
@@ -0,0 +1,21 @@
+return {
+	"CRAG666/code_runner.nvim",
+	dependencies = { "preservim/vimux" },
+	config = function()
+		code_runner = require("code_runner")
+		code_runner.setup({
+			mode = "vimux",
+			filetype = {
+				python = "python3 -u '$dir/$fileName'",
+				sh = "bash",
+			},
+			vim.keymap.set("n", "<localleader>r", ":RunCode<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<localleader>rf", ":RunFile<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<localleader>rft", ":RunFile tab<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<localleader>rp", ":RunProject<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<localleader>rc", ":RunClose<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<localleader>crf", ":CRFiletype<CR>", { noremap = true, silent = false }),
+			vim.keymap.set("n", "<localleader>crp", ":CRProjects<CR>", { noremap = true, silent = false }),
+		})
+	end,
+}
