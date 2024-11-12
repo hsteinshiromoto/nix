e7c9468f1b6db2289319d5329f4308a909531f74
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-12 15:16:25 +1100

    Remove todo-comments nvim plugin as it was freezing nvim

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -68,17 +68,10 @@ return {
 		event = "BufReadPre",
 		opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help" } },
   -- stylua: ignore
-  keys = {
-    { "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
-    { "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
-    { "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
-  },
-	},
-	{
-		"folke/todo-comments.nvim",
-		dependencies = { "nvim-lua/plenary.nvim" },
 		keys = {
-			{ "<leader>ft", "<cmd>TodoTelescope<cr>", desc = "Find ToDos" },
+			{ "<leader>qs", function() require("persistence").load() end, desc = "Restore Session" },
+			{ "<leader>ql", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+			{ "<leader>qd", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
 		},
 	},
 }
