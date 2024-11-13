ee5081995a1d96bfbaf177cb72488e2bdc7e9ad6
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-13 20:47:50 +1100

    Add keymap to auto indent new empty line

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -32,5 +32,15 @@ keymap({ "n", "v" }, "d", '"_d', { desc = "Delete and not yank" })
 -- Autosave and quit
 -- keymap("n", "zz", ":wqa<CR>", { desc = "Save and quit" })
 
+-- Auto Indent the When Inserting New Empty Line[2]
+vim.keymap.set("n", "i", function()
+	if #vim.fn.getline(".") == 0 then
+		return [["_cc]]
+	else
+		return "i"
+	end
+end, { expr = true })
+
 -- References
 -- 	[1] https://alpha2phi.medium.com/modern-neovim-init-lua-ab1220e3ecc1
+-- 	[2] https://alpha2phi.medium.com/modern-neovim-configuration-hacks-93b13283969f
