6fb943961146387f1e30b2a525fd1b2914474536
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-13 14:53:43 +1100

    Changes to todo-comments.lua keymaps
    Change todo keymaps

--- .config/nvim/lua/plugins/todo-comments.lua
+++ .config/nvim/lua/plugins/todo-comments.lua
@@ -8,8 +8,8 @@ return {
     keys = {
       { "]t", function() require("todo-comments").jump_next() end, desc = "Next ToDo" },
       { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous ToDo" },
-      { "<leader>ct", "<cmd>TodoTrouble<cr>", desc = "ToDo (Trouble)" },
-      { "<leader>cT", "<cmd>TodoTelescope<cr>", desc = "ToDo" },
+      { "<leader>bt", "<cmd>TodoTrouble<cr>", desc = "ToDo (Trouble)" },
+      { "<leader>bT", "<cmd>TodoTelescope<cr>", desc = "ToDo" },
     },
 	},
 }
