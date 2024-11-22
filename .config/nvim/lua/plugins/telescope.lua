a7a98be78a4821bf484fb97ee545e64894cce651
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-22 20:32:02 +1100

    Add keymap to Telescope to show keymaps

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -15,6 +15,7 @@ return {
 			{ "<leader>fr", "<cmd>Telescope frecency<cr>", desc = "Recent" },
 			{ "r", "<cmd>Telescope registersk<cr>", desc = "Registers" },
 			{ ",", "<cmd>Telescope buffers<cr>", desc = "Telescope List Buffers" },
+			{ "<leader>k", "<cmd>Telescope keymaps<cr>", desc = "Telescope List Keymaps" },
 			{ "<leader>.", "<cmd>Telescope file_browser<cr>", desc = "Telescope File Browser" },
 			{ "<leader>ps", "<cmd>Telescope repo list<cr>", desc = "Search" },
 			{ "<leader>hs", "<cmd>Telescope help_tags<cr>", desc = "Telescope Tags" },
