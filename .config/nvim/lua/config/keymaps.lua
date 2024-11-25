57ee4bc889b597bf72413df1c567d96e59a11402
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-26 09:32:22 +1100

    Add comments to keymaps

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -34,8 +34,10 @@ keymap({ "n", "v" }, "<del>", '"_d', { desc = "Delete and not yank" })
 -- Autosave and quit
 -- keymap("n", "zz", ":wqa<CR>", { desc = "Save and quit" })
 
+-- Map <leader>backspace to stop Highlight
 keymap({ "n", "v" }, "<leader><BS>", "<cmd>nohlsearch<cr>", { desc = "Stop Highlight" })
 
+-- Map paste above and below current line.
 keymap({ "n", "v" }, "<leader>p", ":pu<CR>", { desc = "Past below line" })
 keymap({ "n", "v" }, "<leader>P", ":pu!<CR>", { desc = "Past above line" })
 
