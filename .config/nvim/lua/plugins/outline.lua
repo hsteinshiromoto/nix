4c854b12f6163df85f0aee485e7fa4650757330c
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-26 21:46:55 +1100

    Add settings for outline plugin

--- .config/nvim/lua/plugins/outline.lua
+++ .config/nvim/lua/plugins/outline.lua
@@ -7,6 +7,15 @@ return {
 
 		require("outline").setup({
 			-- Your setup opts here (leave empty to use defaults)
+			outline_items = {
+				show_symbol_lineno = true,
+			},
+			symbols = {
+				filter = {
+					default = { "String", exclude = true },
+					python = { "Function", "Class" },
+				},
+			},
 		})
 	end,
 }
