0b648b21e1b69d4a44d18155a3eeed9fe7d1080f
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-29 23:21:52 +1100

    Remove relative and absolute number settings from config/options.lua

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,17 +4,12 @@ vim.opt.shiftwidth = indent
 vim.opt.tabstop = indent
 vim.opt.ignorecase = true
 
-
 -- Setup relative and absolute numbers
 -- References:
 --	[1] https://www.reddit.com/r/neovim/comments/14xxw1p/display_both_relative_and_absolute_line_numbers/
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = ";"
-
-vim.opt.nu = true
-vim.opt.relativenumber = true
-vim.o.statuscolumn = "%s %l %r "
 
 -- Setup Neotree to open when NeoVim starts
 -- vim.api.nvim_create_autocmd("VimEnter", {
