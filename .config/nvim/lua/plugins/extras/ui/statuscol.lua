b154009fa848dbaf1944de9f6b152c1cf3e97b7d
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-29 23:01:30 +1100

    Reformat

--- .config/nvim/lua/plugins/extras/ui/statuscol.lua
+++ .config/nvim/lua/plugins/extras/ui/statuscol.lua
@@ -1,51 +1,53 @@
 return {
-	"luukvbaal/statuscol.nvim",
-	lazy = false,
-	config = function()
-		vim.o.number = true
-		vim.o.relativenumber = true
+	{
+		"luukvbaal/statuscol.nvim",
+		lazy = false,
+		config = function()
+			vim.o.number = true
+			vim.o.relativenumber = true
 
-		local builtin = require("statuscol.builtin")
+			local builtin = require("statuscol.builtin")
 
-		require("statuscol").setup({
-			segments = {
-				relculright = true,
+			require("statuscol").setup({
+				segments = {
+					relculright = true,
 
-				-- Absolute line number
-				{
-					text = {
-						function(args)
-							return string.format("%4d ", args.lnum) -- Added space after absolute number
-						end,
+					-- Absolute line number
+					{
+						text = {
+							function(args)
+								return string.format("%4d ", args.lnum) -- Added space after absolute number
+							end,
+						},
+						click = "v:lua.ScLa",
 					},
-					click = "v:lua.ScLa",
-				},
-				-- Relative line number
-				{
-					text = {
-						function(args)
-							return builtin.lnumfunc(args) .. " " -- Added space after relative number
-						end,
+					-- Relative line number
+					{
+						text = {
+							function(args)
+								return builtin.lnumfunc(args) .. " " -- Added space after relative number
+							end,
+						},
+						click = "v:lua.ScLa",
 					},
-					click = "v:lua.ScLa",
-				},
 
-				{
-					sign = { namespace = { "gitsigns" }, name = { ".*" }, maxwidth = 1, colwidth = 2, auto = false },
-					click = "v:lua.ScSa",
+					{
+						sign = { namespace = { "gitsigns" }, name = { ".*" }, maxwidth = 1, colwidth = 2, auto = false },
+						click = "v:lua.ScSa",
+					},
+					{
+						sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
+						click = "v:lua.ScSa",
+					},
+					-- {
+					-- 	text = {
+					-- 		'%{%foldclosed(v:lnum)==v:lnum?"%#Italic#%#DiagnosticVirtualTextWarn#":""%}',
+					-- 		builtin.lnumfunc,
+					-- 	},
+					-- },
+					{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
 				},
-				{
-					sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
-					click = "v:lua.ScSa",
-				},
-				-- {
-				-- 	text = {
-				-- 		'%{%foldclosed(v:lnum)==v:lnum?"%#Italic#%#DiagnosticVirtualTextWarn#":""%}',
-				-- 		builtin.lnumfunc,
-				-- 	},
-				-- },
-				{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-			},
-		})
-	end,
+			})
+		end,
+	},
 }
