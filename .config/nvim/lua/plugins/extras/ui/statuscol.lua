af00eb9b14a6a04c800d6f6efb835bdef127d863
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-30 21:41:05 +1100

    Add todo

--- .config/nvim/lua/plugins/extras/ui/statuscol.lua
+++ .config/nvim/lua/plugins/extras/ui/statuscol.lua
@@ -9,6 +9,8 @@ return {
 			"kevinhwang91/promise-async",
 			"neovim/nvim-lspconfig",
 			{
+				-- FIX This plugin is rainins the error message: E36: Not enought room.
+				--
 				-- TODO: It looks like gitsigns has a priority over this plygin, as it does not highlight lines highlighted by gitsigns. Investigate how it can highlight the line number column.
 				--
 				-- TODO Change pallete to Gruvbox
