7138e132e6cb5f1103a0a0fbb6f8e6d85da90a61
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-30 14:24:46 +1100

    Add clickable folds into status column

--- .config/nvim/lua/plugins/extras/ui/statuscol.lua
+++ .config/nvim/lua/plugins/extras/ui/statuscol.lua
@@ -65,6 +65,13 @@ return {
 			},
 		},
 		event = "BufReadPost",
+		init = function()
+			vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+			vim.o.foldcolumn = "1" -- '0' is not bad
+			vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+			vim.o.foldlevelstart = 99
+			vim.o.foldenable = true
+		end,
 		opts = {
 			provider_selector = function()
 				return { "treesitter", "indent" }
