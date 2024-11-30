845bbc92a9c66f233158aab4a6701b01a0bc8cd0
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-30 12:47:03 +1100

    Add commented diagnostics col

--- .config/nvim/lua/plugins/extras/ui/statuscol.lua
+++ .config/nvim/lua/plugins/extras/ui/statuscol.lua
@@ -11,13 +11,24 @@ return {
 				vim.opt.number = true
 				-- vim.opt.relativenumber = true
 				local builtin = require("statuscol.builtin")
+
 				require("statuscol").setup({
 					relculright = true,
 					segments = {
 						{ text = { builtin.foldfunc }, click = "v:lua.ScFa" },
 						{ text = { "%s" }, colwidth = 1, maxwidth = 1, auto = false, click = "v:lua.ScSa" }, -- git signs
 						{ text = { "%=", "%l " }, click = "v:lua.ScLa" }, -- Absolute line numbers
+
 						-- { text = { "%r " }, click = "v:lua.ScLa" }, -- Relative line numbers
+						-- FIX: Bugs with the following code block. Need to add Diagnostic.
+						-- {
+						-- 	sign = {
+						-- 		namespace= { "Diagnostic" },
+						-- 		maxwidth = 2,
+						-- 		auto = true,
+						-- 	},
+						-- 	click = "v:lua.ScSa",
+						-- },
 					},
 				})
 			end,
