bb50f6937342ce6a15a09ccee3c74e097e68a229
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-11-30 14:38:09 +1100

    Improve fold to add also number of folded lines

--- .config/nvim/lua/plugins/extras/ui/statuscol.lua
+++ .config/nvim/lua/plugins/extras/ui/statuscol.lua
@@ -64,6 +64,7 @@ return {
 				end,
 			},
 		},
+		-- The following settings for nvim-ufo are from [2].
 		init = function()
 			vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 			vim.o.foldcolumn = "auto:1" -- '0' is not bad [1]
@@ -72,12 +73,74 @@ return {
 			vim.o.foldenable = true
 		end,
 		opts = {
-			provider_selector = function()
-				return { "treesitter", "indent" }
-			end,
+			-- INFO: Uncomment to use treeitter as fold provider, otherwise nvim lsp is used
+			-- provider_selector = function(bufnr, filetype, buftype)
+			--   return { "treesitter", "indent" }
+			-- end,
+			open_fold_hl_timeout = 400,
+			close_fold_kinds = { "imports", "comment" },
+			preview = {
+				win_config = {
+					border = { "", "─", "", "", "", "─", "", "" },
+					winhighlight = "Normal:Folded",
+					winblend = 0,
+				},
+				mappings = {
+					scrollU = "<C-k>",
+					scrollD = "<C-j>",
+					jumpTop = "[",
+					jumpBot = "]",
+				},
+			},
 		},
+		config = function(_, opts)
+			local handler = function(virtText, lnum, endLnum, width, truncate)
+				local newVirtText = {}
+				local totalLines = vim.api.nvim_buf_line_count(0)
+				local foldedLines = endLnum - lnum
+				local suffix = ("  %d %d%%"):format(foldedLines, foldedLines / totalLines * 100)
+				local sufWidth = vim.fn.strdisplaywidth(suffix)
+				local targetWidth = width - sufWidth
+				local curWidth = 0
+				for _, chunk in ipairs(virtText) do
+					local chunkText = chunk[1]
+					local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+					if targetWidth > curWidth + chunkWidth then
+						table.insert(newVirtText, chunk)
+					else
+						chunkText = truncate(chunkText, targetWidth - curWidth)
+						local hlGroup = chunk[2]
+						table.insert(newVirtText, { chunkText, hlGroup })
+						chunkWidth = vim.fn.strdisplaywidth(chunkText)
+						-- str width returned from truncate() may less than 2nd argument, need padding
+						if curWidth + chunkWidth < targetWidth then
+							suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth)
+						end
+						break
+					end
+					curWidth = curWidth + chunkWidth
+				end
+				local rAlignAppndx = math.max(math.min(vim.opt.textwidth["_value"], width - 1) - curWidth - sufWidth, 0)
+				suffix = (" "):rep(rAlignAppndx) .. suffix
+				table.insert(newVirtText, { suffix, "MoreMsg" })
+				return newVirtText
+			end
+			opts["fold_virt_text_handler"] = handler
+			require("ufo").setup(opts)
+			vim.keymap.set("n", "zR", require("ufo").openAllFolds)
+			vim.keymap.set("n", "zM", require("ufo").closeAllFolds)
+			vim.keymap.set("n", "zr", require("ufo").openFoldsExceptKinds)
+			vim.keymap.set("n", "K", function()
+				local winid = require("ufo").peekFoldedLinesUnderCursor()
+				if not winid then
+					-- vim.lsp.buf.hover()
+					vim.cmd([[ Lspsaga hover_doc ]])
+				end
+			end)
+		end,
 	},
 	{ "anuvyklack/fold-preview.nvim", dependencies = "anuvyklack/keymap-amend.nvim", config = true, lazy = false },
 }
 -- References:
 -- 	[1] https://github.com/kevinhwang91/nvim-ufo/issues/4#issuecomment-2241159750
+-- 	[2] https://github.com/kevinhwang91/nvim-ufo/issues/4#issuecomment-1514537245
