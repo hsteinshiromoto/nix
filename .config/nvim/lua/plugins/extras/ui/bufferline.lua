12b02d0c0edb0363278ab4ca2889aa275ea14d9d
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-12-01 21:35:24 +1100

    Add bufferline nvim plugin

---
+++ .config/nvim/lua/plugins/extras/ui/bufferline.lua
@@ -0,0 +1,23 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	lazy = false,
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		vim.opt.termguicolors = true
+		local bufferline = require("bufferline")
+		bufferline.setup({
+			options = {
+				mode = "tabs",
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+						text_align = "center",
+						separator = true,
+					},
+				},
+			},
+		})
+	end,
+}
