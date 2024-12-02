9a713898c7933fa3c5fad88bfa00d80bef00fe7a
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-12-02 21:41:27 +1100

    Fixbug with cursor flickering

--- .config/nvim/lua/plugins/extras/ui/statusline/init.lua
+++ .config/nvim/lua/plugins/extras/ui/statusline/init.lua
@@ -29,7 +29,8 @@ return {
 					lualine_b = {
 						components.workspace,
 						{ "branch", icon = icons.git.Branch },
-						components.git_repo,
+						-- FIX: components.git_repo is making the cursor to flicker
+						-- components.git_repo,
 						components.diff,
 					},
 					lualine_c = { components.diagnostics, components.lsp_client },
