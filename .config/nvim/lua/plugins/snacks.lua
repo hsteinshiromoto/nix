8fd1b8d8b7c748fbf22129890b3f90fb24c0751a
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-12-09 22:28:26 +1100

    Add snacks.lua

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,39 @@
+return {
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		bigfile = { enabled = true },
+		lazygit = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		statuscolumn = { enabled = true },
+		words = { enabled = true },
+	},
+	keys = {
+		{
+			"gl",
+			function()
+				Snacks.lazygit()
+			end,
+			desc = "Lazygit",
+		},
+		{
+			"<leader>gf",
+			function()
+				Snacks.lazygit.log_file()
+			end,
+			desc = "Lazygit Current File History",
+		},
+		{
+			"<leader>rf",
+			function()
+				Snacks.rename.rename_file()
+			end,
+			desc = "Rename File",
+		},
+	},
+}
