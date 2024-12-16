8f2b4396d4aece10d5c7fe1deca31cc87b9c494b
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-12-16 20:50:12 +1100

    Add note id to aliases field

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -101,6 +101,29 @@ return {
 			local path = spec.dir / tostring(basename)
 			return path:with_suffix(".md")
 		end,
+		-- Optional, alternatively you can customize the frontmatter data.
+		---@return table
+		note_frontmatter_func = function(note)
+			-- Add the title of the note as an alias.
+			if note.title then
+				note:add_alias(note.title)
+			end
+			if note.id then
+				note:add_alias(note.id)
+			end
+
+			local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+
+			-- `note.metadata` contains any manually added fields in the frontmatter.
+			-- So here we just make sure those fields are kept in the frontmatter.
+			if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+				for k, v in pairs(note.metadata) do
+					out[k] = v
+				end
+			end
+
+			return out
+		end,
 	},
 }
 -- References:
