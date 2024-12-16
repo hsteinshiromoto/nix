237321b7b1f23e633e4254b3befa69b348c90c45
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-12-16 19:42:40 +1100

    obsidian.nvim now setups a random string for id

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -9,6 +9,22 @@ local function make_id()
 	end
 	return tostring(os.date("%Y-%m-%d")) .. "_" .. suffix
 end
+local function basename_func(title)
+	-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+	-- In this case a note with the title 'My new note' will be given an ID that looks
+	-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
+	local suffix = ""
+	if title ~= nil then
+		-- If title is given, transform it into valid file name.
+		suffix =
+			title:gsub(" ", "_"):gsub("[^A-Za-z0-9-]", " "):gsub("(%a)([%w_']*)", tchelper):gsub("[^A-Za-z0-9-]", "_")
+		suffix = tostring(os.date("%Y-%m-%d")) .. "_" .. suffix
+	else
+		-- If title is nil, just add 4 random uppercase letters to the suffix.
+		suffix = make_id()
+	end
+	return suffix
+end
 
 return {
 	"epwalsh/obsidian.nvim",
@@ -76,33 +92,13 @@ return {
 		note_id_func = function()
 			return make_id()
 		end,
-		---@param title string|?
-		---@return string
-		global basename_func = function(title)
-			-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-			-- In this case a note with the title 'My new note' will be given an ID that looks
-			-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
-			local suffix = ""
-			if title ~= nil then
-				-- If title is given, transform it into valid file name.
-				suffix = title
-					:gsub(" ", "_")
-					:gsub("[^A-Za-z0-9-]", " ")
-					:gsub("(%a)([%w_']*)", tchelper)
-					:gsub("[^A-Za-z0-9-]", "_")
-				suffix = tostring(os.date("%Y-%m-%d")) .. "_" .. suffix
-			else
-				-- If title is nil, just add 4 random uppercase letters to the suffix.
-				suffix = make_id()
-			end
-			return suffix
-		end,
 		-- Optional, customize how note file names are generated given the ID, target directory, and title.
 		---@param spec { id: string, dir: obsidian.Path, title: string|? }
 		---@return string|obsidian.Path The full path to the new note.
 		note_path_func = function(spec)
+			local basename = basename_func(spec.title)
 			-- This is equivalent to the default behavior.
-			local path = spec.dir / tostring(spec.id)
+			local path = spec.dir / tostring(basename)
 			return path:with_suffix(".md")
 		end,
 	},
