8a2de6d90f2d1d73c0be71f33505e1f5fd49aa34
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-12-16 19:37:48 +1100

    nvim obsidian plugin is operational

--- .config/nvim/lua/plugins/obsidian.lua
+++ .config/nvim/lua/plugins/obsidian.lua
@@ -72,9 +72,13 @@ return {
 			substitutions = {},
 		},
 		-- Optional, customize how note IDs are generated given an optional title.
+		---@return string
+		note_id_func = function()
+			return make_id()
+		end,
 		---@param title string|?
 		---@return string
-		note_id_func = function(title)
+		global basename_func = function(title)
 			-- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
 			-- In this case a note with the title 'My new note' will be given an ID that looks
 			-- like '1657296016-my-new-note', and therefore the file name '1657296016-my-new-note.md'
@@ -86,11 +90,12 @@ return {
 					:gsub("[^A-Za-z0-9-]", " ")
 					:gsub("(%a)([%w_']*)", tchelper)
 					:gsub("[^A-Za-z0-9-]", "_")
+				suffix = tostring(os.date("%Y-%m-%d")) .. "_" .. suffix
 			else
 				-- If title is nil, just add 4 random uppercase letters to the suffix.
 				suffix = make_id()
 			end
-			return tostring(os.date("%Y-%m-%d")) .. "_" .. suffix
+			return suffix
 		end,
 		-- Optional, customize how note file names are generated given the ID, target directory, and title.
 		---@param spec { id: string, dir: obsidian.Path, title: string|? }
