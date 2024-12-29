622590ff1479da369cbf9a65f7339b97ab92fea7
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-12-29 11:34:00 +1100

    Move gitsigns to its own file

--- .config/nvim/lua/plugins/extras/ui/statuscol/init.lua
+++ .config/nvim/lua/plugins/extras/ui/statuscol/init.lua
@@ -33,32 +33,6 @@ return {
 					})
 				end,
 			},
-			{
-				"lewis6991/gitsigns.nvim",
-				lazy = false,
-				config = function()
-					require("gitsigns").setup({
-						signs = {
-							add = { text = icons.ui.ThickLeftLine .. "+" },
-							change = { text = icons.ui.ThickLeftLine .. icons.ui.PlusMinus },
-							delete = { text = icons.ui.ThickLeftLine .. "-" },
-							topdelete = { text = icons.ui.ThickLeftLine .. icons.ui.ChevronUp },
-							changedelete = { text = icons.ui.ThickLeftLine .. icons.ui.Cross },
-							untracked = { text = "┆" },
-						},
-						signs_staged = {
-							add = { text = icons.ui.ThickLeftLine .. "+" },
-							change = { text = icons.ui.ThickLeftLine .. icons.ui.PlusMinus },
-							delete = { text = icons.ui.ThickLeftLine .. "-" },
-							topdelete = { text = icons.ui.ThickLeftLine .. icons.ui.ChevronUp },
-							changedelete = { text = icons.ui.ThickLeftLine .. icons.ui.Cross },
-							untracked = { text = "┆" },
-						},
-						-- Highlight also the line number
-						numhl = true,
-					})
-				end,
-			},
 		},
 		-- The following settings for nvim-ufo are from [2].
 		init = function()
