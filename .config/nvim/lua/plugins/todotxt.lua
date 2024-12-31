4c58383c9ee501c130d83f5f26b15caa7908775b
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2024-12-31 22:09:54 +1100

    Setup todo.txt priorities and add comments about how to use

--- .config/nvim/lua/plugins/todotxt.lua
+++ .config/nvim/lua/plugins/todotxt.lua
@@ -4,6 +4,27 @@ return {
 	config = function()
 		require("todotxt-nvim").setup({
 			todo_file = "todo.txt",
+			alternative_priority = {
+				A = "now",
+				B = "next",
+				C = "this week",
+				D = "next week",
+				E = "next fortnight",
+			},
 		})
 	end,
 }
+-- Format Rules [2]:
+-- х (А) 2016-05-20 2016-04-30 measure space for +chapelShelving @chapel due:2016-05-30
+-- │ 	│  │ 					│ 				 │ 								 │ 							 │ 			 └──> Special key:value tag
+-- │ 	│  │ 					│ 				 │ 								 │ 							 └──────────> Context tag
+-- │ 	│  │ 					│ 				 │ 								 └──────────────────────────> Project tag
+-- │ 	│  │ 					│ 				 └────────────────────────────────────────────> Description with tags
+-- │ 	│  │ 			  	└───────────────────────────────────────────────────────> Creation Date
+-- │ 	│  └──────────────────────────────────────────────────────────────────> Completion Date
+-- │ 	└─────────────────────────────────────────────────────────────────────> Priority (opt.)
+-- └────────────────────────────────────────────────────────────────────────> Completition Marker (opt.)
+--
+-- References:
+-- 	[1] https://github.com/arnarg/todotxt.nvim
+-- 	[2] https://github.com/todotxt/todo.txt
