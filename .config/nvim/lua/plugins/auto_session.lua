82d09990705171d071e992cacf16cd5332ee1396
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2025-01-03 19:30:43 +1100

    add auto-session plugin

---
+++ .config/nvim/lua/plugins/auto_session.lua
@@ -0,0 +1,8 @@
+return {
+	"rmagatti/auto-session",
+	lazy = false,
+	opts = {
+		suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "~/Desktop", "~/Documents", "~/Pictures", "~/Public" },
+		-- log_level = 'debug',
+	},
+}
