5e93585140a73ee1fb32eba0b4a96528babd0a68
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2025-01-07 16:12:37 +1100

    Use snacks' dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -6,6 +6,16 @@ return {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		dashboard = {
+			enabled = true,
+			sections = {
+				{ section = "header" },
+				{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
+				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+				{ section = "startup" },
+			},
+		},
 		dim = { enabled = true },
 		bigfile = { enabled = true },
 		bufdelete = { enabled = true },
