6b7dd01e44ffd519bcaaaef2773fa09172a94693
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2025-02-25 21:33:53 +1100

    Changes to mini.surround nvim plugin
    Setup key sas to add wiki links by adding the [[ ... ]] surrouds in
    selected characters of visual mode

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -3,7 +3,16 @@ return {
 	version = "*",
 	event = "VeryLazy",
 	config = function()
-		require("mini.surround").setup()
+		require("mini.surround").setup({
+			-- Add custom surroundings to be used on top of builtin ones. For more
+			-- information with examples, see `:h MiniSurround.config`.
+			custom_surroundings = {
+				s = {
+					input = { "%[%[().-()%]%]" },
+					output = { left = "[[", right = "]]" },
+				},
+			},
+		})
 	end,
 }
 -- Examples:
