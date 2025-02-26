d9910252fce98a80eae7882041f76db4410513b4
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2025-02-26 23:14:15 +1100

    enable snacks explorer

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -2,10 +2,13 @@ return {
 	"folke/snacks.nvim",
 	priority = 1000,
 	lazy = false,
+	---@type snacks.Config
 	opts = {
 		-- your configuration comes here
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
+		bigfile = { enabled = true },
+		bufdelete = { enabled = true },
 		dashboard = {
 			enabled = true,
 			sections = {
@@ -38,8 +41,7 @@ return {
 			},
 		},
 		dim = { enabled = true },
-		bigfile = { enabled = true },
-		bufdelete = { enabled = true },
+		explorer = { enabled = true },
 		lazygit = {
 			dependencies = {
 				"nvim-lua/plenary.nvim",
