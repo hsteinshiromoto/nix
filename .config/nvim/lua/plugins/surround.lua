51a828cd2ffc4fa72b699366c9cab626f8360955
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-03-08 21:21:49 +1100

    Improve mini.surround

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -14,6 +14,9 @@ return {
 			},
 		})
 	end,
+	-- Note: if 'timeoutlen' is low enough to cause occasional usage of |s| key
+	-- (that deletes character under cursor), disable it with the following call:
+	vim.keymap.set({ "n", "x" }, "s", "<Nop>"),
 }
 -- Examples:
 -- | keymap | pre-execution | post-execution |
