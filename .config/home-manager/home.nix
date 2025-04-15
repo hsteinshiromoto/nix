c5bfb799302e3eaac2993912dda5ac6d9e94f294
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2025-04-15 19:32:45 +1000

    Add lazygit to home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -22,7 +22,7 @@
     # # "Hello, world!" when run.
     pkgs.hello
 		pkgs.tmux
-
+		pkgs.lazygit
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
