f4f9b5bdbf99c79c7d92d605da1228925d472af0
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2025-04-15 21:09:15 +1000

    Reformat

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -20,18 +20,20 @@
   home.packages = [
     # # Adds the 'hello' command to your environment. It prints a friendly
     # # "Hello, world!" when run.
+		pkgs.bat
 		pkgs.eza
-    pkgs.hello
-		pkgs.tmux
-		pkgs.lazygit
-		pkgs.zsh
     pkgs.fzf
+    pkgs.hello
+		pkgs.lazygit
+		pkgs.nerd-fonts.jetbrains-mono
+		pkgs.tmux
     pkgs.zoxide
+		pkgs.zsh
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
-		pkgs.nerd-fonts.jetbrains-mono
+
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
