6ccc63d63ee5349a092424ebbe97aab34389b830
Humberto STEIN SHIROMOTO <h.stein.shiromoto@gmail.com>
2025-04-19 23:03:42 +1000

    Add function to read .zshrc

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -116,6 +116,7 @@
       if [ -e '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh' ]; then
         . '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
       fi
+			builtins.readFile ./zshrc;
     '';
 	};
 	programs.git = {
