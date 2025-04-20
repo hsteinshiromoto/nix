9b544e7b63dd2434c126823dbbaf5e8780816f72
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-04-20 22:00:03 +1000

    Pass homedirectory as parameter from flake.nix to home.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -1,10 +1,10 @@
-{ config, pkgs, ... }:
+{ config, pkgs, homeDirectory, ... }:
 
 {
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "hsteinshiromoto";
-  home.homeDirectory = "/home/hsteinshiromoto";
+  home.homeDirectory = homeDirectory;
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
