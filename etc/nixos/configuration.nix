c1b8248170f94dbac3ed1c1050f69e8ff60c9fea
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-04-26 20:25:48 +1000

    Add neovim and lazygit to configuration.nix

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -66,6 +66,8 @@ in
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     git
+    lazygit
+    neovim
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
