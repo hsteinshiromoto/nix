6f957a41c77e77e14029838bcdde0c62408eb8bc
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-05-10 19:23:54 +1000

    Improve formatting of the networking

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -20,15 +20,15 @@ in
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "servidor"; # Define your hostname.
+  networking = {
+		hostName = "servidor";
+		networkmanager.enable = true;
+	}; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Set your time zone.
   time.timeZone = "Australia/Sydney";
