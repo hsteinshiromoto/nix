be1d9ca6067069e47ba8dfe7c04fb76f9bca6cc0
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-05-13 21:30:42 +1000

    Fix bug with network manager

--- custom_iso.nix
+++ custom_iso.nix
@@ -5,4 +5,10 @@
     <nixpkgs/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix>
     etc/nixos/configuration.nix
   ];
+
+	# Explicitly override any wireless settings from other modules
+  networking.wireless.enable = false;
+
+  # Ensure NetworkManager is enabled (though this is already in your configuration.nix)
+  networking.networkmanager.enable = true;
 }
