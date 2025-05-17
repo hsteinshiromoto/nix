5c5db98f6a91f86722c4ef905a44b773d9413da8
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-05-17 19:10:16 +1000

    Add gabage collector settings

--- servo/configuration.nix
+++ servo/configuration.nix
@@ -15,6 +15,12 @@
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+
+		gc = {
+			automatic = true;
+			dates = "weekly";
+			options = "--delete-older-than +5";
+		};
   };
 
   # Bootloader.
