bcc2807e865454e420d50f960aa5a5709b6a7581
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-06-04 20:46:35 +1000

    Add atuin

--- servo/configuration.nix
+++ servo/configuration.nix
@@ -97,6 +97,7 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+		atuin
     autoconf        # Build essential
     automake        # Build essential
     bat
