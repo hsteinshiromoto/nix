4a77d22b17b8e479be37cb87dac1766f508d979c
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-06-30 20:14:52 +1000

    Remove home manager

--- flake.nix
+++ flake.nix
@@ -13,12 +13,6 @@
     };
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
 
-    # Home Manager
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
