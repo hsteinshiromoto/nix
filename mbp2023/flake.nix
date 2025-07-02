55bed8bf54e681fb0550672efd03fb19139466d2
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-07-02 20:01:19 +1000

    Add comment

--- mbp2023/flake.nix
+++ mbp2023/flake.nix
@@ -116,7 +116,8 @@
       # $ darwin-rebuild changelog
       system.stateVersion = 6;
 
-			# The following user definition is required by home-manager
+			# The following user definition is required by home-manager [1]
+			# [1] https://discourse.nixos.org/t/homedirectory-is-note-of-type-path-darwin/57453/6
 			users.users.hsteinshiromoto = {
 				name = "hsteinshiromoto";
 				home = "/Users/hsteinshiromoto";
