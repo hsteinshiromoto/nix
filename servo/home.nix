f1d7f114e3111ea4a949f4b71084fedec3c7bff8
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-07-12 21:22:18 +1000

    Add sops settings in home.nix

--- servo/home.nix
+++ servo/home.nix
@@ -10,6 +10,22 @@
     age
   ];
 
+	# Adding sops
+	# References:
+	#		[1] https://zohaib.me/managing-secrets-in-nixos-home-manager-with-sops/
+	sops = {
+    age.keyFile = "/home/hsteinshiromoto/.config/sops/age/keys.txt"; # must have no password!
+
+    defaultSopsFile = ./secrets.yaml;
+    defaultSymlinkPath = "/run/user/1000/secrets";
+    defaultSecretsMountPoint = "/run/user/1000/secrets.d";
+
+    secrets.openai_api_key = {
+      # sopsFile = ./secrets.yml.enc; # optionally define per-secret files
+      path = "${config.sops.defaultSymlinkPath}/openai_api_key";
+    };
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
