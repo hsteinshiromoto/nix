a4c021a4b6155fbb96dac574976f776de556c36f
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2025-07-16 19:25:02 +1000

    Add bartender and maccy to mbp2025 flake

--- mbp2025/flake.nix
+++ mbp2025/flake.nix
@@ -46,6 +46,7 @@
 			"neovim"
 		];
 		casks = [
+			"bastender"
 			"cursor"
 			"docker-desktop"
 			"drawio"
@@ -53,7 +54,7 @@
 			"firefox"
 			"ghostty"
 			"google-chrome"
-			"hiddenbar"
+			"maccy"
 			"microsoft-teams"
 			"obsidian"
 			"popclip"
