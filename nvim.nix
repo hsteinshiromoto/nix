677568b4eb801065ca68c9fbb7d72bb3c9d3791f
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-10-21 21:57:20 +1100

    Add several ls

--- nvim.nix
+++ nvim.nix
@@ -2,8 +2,12 @@
 
 {
   environment.systemPackages = with pkgs; [
+		docker-language-server
     lua-language-server
     marksman
+		nodePackages_latest.vscode-json-languageserver
+		systemd-language-server
     texlab
+		yaml-language-server
   ];
 }
