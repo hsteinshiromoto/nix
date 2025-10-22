9999b08561aec67ae74c2ffb86d5d1c15241049e
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2025-10-22 13:54:48 +1100

    PROJ-123: Fix issue with integratino of starship and atuin with nushell

--- mbp2025/home.nix
+++ mbp2025/home.nix
@@ -150,6 +150,28 @@ git_protocol: https
 		};
 	};
 
+	# Create tool integration init files
+	xdg.configFile."nushell-integrations/starship.nu".text = ''
+		${builtins.readFile (pkgs.runCommand "starship-init" {} ''
+			${pkgs.starship}/bin/starship init nu > $out
+		'')}
+	'';
+
+	xdg.configFile."nushell-integrations/zoxide.nu".text = ''
+		${builtins.readFile (pkgs.runCommand "zoxide-init" {} ''
+			${pkgs.zoxide}/bin/zoxide init nushell > $out
+		'')}
+	'';
+
+	xdg.configFile."nushell-integrations/atuin.nu".text = ''
+		${builtins.readFile (pkgs.runCommand "atuin-init" {
+			HOME = "/tmp/atuin-build-home";
+		} ''
+			mkdir -p $HOME/.config/atuin
+			${pkgs.atuin}/bin/atuin init nu > $out
+		'')}
+	'';
+
 	# Manually create nushell config files using xdg.configFile
 	xdg.configFile."nushell/config.nu".text = ''
 		# Nushell configuration
@@ -167,6 +189,11 @@ git_protocol: https
 		alias get_arn = aws sts get-caller-identity --query Arn --output text
 		alias cat = bat
 		alias get_aws_id = aws sts get-caller-identity | from json
+
+		# Tool integrations
+		source ~/.config/nushell-integrations/starship.nu
+		source ~/.config/nushell-integrations/zoxide.nu
+		source ~/.config/nushell-integrations/atuin.nu
 	'';
 
 	xdg.configFile."nushell/env.nu".text = ''
