5957c71b4b2c7b537e6cfaa58eb678973ea8ba5e
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2025-10-24 13:42:22 +1100

    PROJ-123: Add common/sops.nix

---
+++ common/sops.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  # SOPS configuration for home-manager
+  sops = {
+    # Use GPG for decryption
+    gnupg.home = "${config.home.homeDirectory}/.gnupg";
+
+    # Disable build-time validation to avoid sandbox permission issues on Darwin
+    validateSopsFiles = false;
+
+    # No defaultSopsFile - each secret will specify its own sopsFile
+    # This allows loading from multiple files in the secrets folder:
+    # - ~/.config/sops/secrets/gitconfig.yaml
+    # - ~/.config/sops/secrets/gitlab.yaml (if needed)
+    # - etc.
+  };
+}
