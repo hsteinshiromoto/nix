6e0be8e8650efb88150b58ae018fce09b004d06f
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2025-10-24 13:55:46 +1100

    PROJ-123: Remove comment

--- common/sops.nix
+++ common/sops.nix
@@ -8,11 +8,5 @@
 
     # Disable build-time validation to avoid sandbox permission issues on Darwin
     validateSopsFiles = false;
-
-    # No defaultSopsFile - each secret will specify its own sopsFile
-    # This allows loading from multiple files in the secrets folder:
-    # - ~/.config/sops/secrets/gitconfig.yaml
-    # - ~/.config/sops/secrets/gitlab.yaml (if needed)
-    # - etc.
   };
 }
