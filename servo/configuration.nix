53680f928e801bcb0629c13f5dd7f700d0eb6501
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2025-12-21 21:39:44 +1100

    Add wireless tools package in servo

--- servo/configuration.nix
+++ servo/configuration.nix
@@ -176,6 +176,7 @@
 		yubikey-agent
     yq
     wget
+		wirelesstools
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
