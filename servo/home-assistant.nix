e85784ecee14e06a034ad8227e5418116ac1430c
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2026-01-19 21:43:39 +1100

    Add nginx for home-assistant

--- servo/home-assistant.nix
+++ servo/home-assistant.nix
@@ -1,31 +1,49 @@
 { config, lib, pkgs, ... }:
 
 {
-	services.home-assistant = {
-    enable = true;
-    extraComponents = [
-      # Components required to complete the onboarding
-      "analytics"
-      "google_translate"
-      "met"
-      "radio_browser"
-      "shopping_list"
-      # Recommended for fast zlib compression
-      # https://www.home-assistant.io/integrations/isal
-      "isal"
-      # Bluetooth support
-      "bluetooth"
-      "bluetooth_le_tracker"
-      # Philips Hue integration
-      "hue"
-    ];
-    extraPackages = python3Packages: with python3Packages; [
-      bleak
-    ];
-    config = {
-      # Includes dependencies for a basic setup
-      # https://www.home-assistant.io/integrations/default_config/
-      default_config = {};
-    };
-  };
+	services = {
+		home-assistant = {
+			enable = true;
+			extraComponents = [
+				# Components required to complete the onboarding
+				"analytics"
+				"met"
+				"radio_browser"
+				# Recommended for fast zlib compression
+				# https://www.home-assistant.io/integrations/isal
+				"isal"
+				# Bluetooth support
+				"bluetooth"
+				"bluetooth_le_tracker"
+				# Philips Hue integration
+				"hue"
+			];
+			extraPackages = python3Packages: with python3Packages; [
+				bleak
+			];
+			config = {
+				# Includes dependencies for a basic setup
+				# https://www.home-assistant.io/integrations/default_config/
+				server_host = "::1";
+				trusted_proxies = [ "::1" ];
+				use_x_forwarded_for = true;
+			};
+		};
+
+		nginx = {
+			enable = true;
+			recommendedProxySettings = true;
+			virtualHosts."servidor" = {
+				forceSSL = true;
+				enableACME = true;
+				extraConfig = ''
+					proxy_buffering off;
+				'';
+				locations."/" = {
+					proxyPass = "http://[::1]:8123";
+					proxyWebsockets = true;
+				};
+			};
+		};
+	};
 }
