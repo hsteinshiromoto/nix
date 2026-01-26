b5aeaeb9068e1d198e676ee4c23c34627fcbe142
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2026-01-26 22:01:45 +1100

    Add transmission

---
+++ servo/transmission.nix
@@ -0,0 +1,30 @@
+# Transmission BitTorrent client configuration
+{ config, pkgs, ... }:
+
+let
+  downloadDir = "/mnt/media/Downloads";
+in {
+  # Create download directories with proper permissions
+  systemd.tmpfiles.rules = [
+    "d ${downloadDir} 0775 transmission transmission -"
+    "d ${downloadDir}/.incomplete 0775 transmission transmission -"
+  ];
+
+  services.transmission = {
+    enable = true;
+    openRPCPort = true;
+    openPeerPorts = true;
+    settings = {
+      download-dir = downloadDir;
+      incomplete-dir-enabled = true;
+      incomplete-dir = "${downloadDir}/.incomplete";
+      rpc-bind-address = "0.0.0.0";
+      rpc-whitelist-enabled = true;
+      rpc-whitelist = "127.0.0.1,100.64.*.*";
+    };
+  };
+
+  # Ensure transmission starts after media drive is mounted
+  systemd.services.transmission.after = [ "mnt-media.mount" ];
+  systemd.services.transmission.requires = [ "mnt-media.mount" ];
+}
