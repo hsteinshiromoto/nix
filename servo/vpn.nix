68ed6a47d170d76094095d9a1fa846dcf8065b2d
Humberto STEIN SHIROMOTO <hsteinshiromoto@gmail.com>
2026-02-05 22:04:42 +1100

    Use SOPS for VPN credentials
    SOPS-managed secret for ProtonVPN  username to improve security and
    consistency with other secrets.

--- servo/vpn.nix
+++ servo/vpn.nix
@@ -5,13 +5,20 @@
     openvpn
   ];
 
+  sops.secrets."vpn_username" = {
+    sopsFile = /home/hsteinshiromoto/.config/sops/secrets/servidor/vpn.yaml;
+    key = "username";
+    mode = "0400";
+    owner = "root";
+  };
+
   services.openvpn.servers = {
     protonvpn = {
       config = ''
         config /home/hsteinshiromoto/.vpn/protonvpn-config.ovpn
       '';
       authUserPass = {
-        username = "/etc/nixos/protonvpn.auth";
+        username = config.sops.secrets."vpn_username".path;
       };
       autoStart = false;
     };
