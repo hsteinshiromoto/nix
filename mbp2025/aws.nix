c0830980edf52b19a5a66d5a4c1815db2f32015a
Humberto STEIN SHIROMOTO <humberto.shiromoto@akordi.com>
2026-02-05 12:12:05 +1100

    Add missing fields from aws config files

--- mbp2025/aws.nix
+++ mbp2025/aws.nix
@@ -1,83 +1,78 @@
 { config, pkgs, ... }:
 
+let
+  awsSopsFile = "${config.home.homeDirectory}/.config/sops/secrets/mbp2025/aws.yaml";
+in
 {
-  # SOPS secrets configuration for AWS
   sops = {
     secrets = {
-      # Config profile secrets
-      "config/test/account_id" = {
-        sopsFile = "${config.home.homeDirectory}/.config/sops/secrets/mbp2025/aws.yaml";
-      };
-      "config/qa02/account_id" = {
-        sopsFile = "${config.home.homeDirectory}/.config/sops/secrets/mbp2025/aws.yaml";
-      };
-      "config/dev02/account_id" = {
-        sopsFile = "${config.home.homeDirectory}/.config/sops/secrets/mbp2025/aws.yaml";
-      };
-      "config/sandbox/account_id" = {
-        sopsFile = "${config.home.homeDirectory}/.config/sops/secrets/mbp2025/aws.yaml";
-      };
-      "config/produk/account_id" = {
-        sopsFile = "${config.home.homeDirectory}/.config/sops/secrets/mbp2025/aws.yaml";
-      };
-      "config/sso_session_id" = {
-        sopsFile = "${config.home.homeDirectory}/.config/sops/secrets/mbp2025/aws.yaml";
-      };
-      "config/sso_start_url" = {
-        sopsFile = "${config.home.homeDirectory}/.config/sops/secrets/mbp2025/aws.yaml";
-      };
-      # SSM tunnel secrets - produk
-      "ssm/produk/uk/host" = {
-        sopsFile = "${config.home.homeDirectory}/.config/sops/secrets/mbp2025/aws.yaml";
-      };
-      "ssm/produk/uk/port" = {
-        sopsFile = "${config.home.homeDirectory}/.config/sops/secrets/mbp2025/aws.yaml";
-      };
-      "ssm/produk/uk/local_port" = {
-        sopsFile = "${config.home.homeDirectory}/.config/sops/secrets/mbp2025/aws.yaml";
-      };
+      # SSO session config
+      "config/sso_session_id".sopsFile = awsSopsFile;
+      "config/sso_start_url".sopsFile = awsSopsFile;
+      "config/sso_region".sopsFile = awsSopsFile;
+      "config/sso_registration_scopes".sopsFile = awsSopsFile;
+      "config/default_region".sopsFile = awsSopsFile;
+      # Profile secrets
+      "config/test/account_id".sopsFile = awsSopsFile;
+      "config/test/role_name".sopsFile = awsSopsFile;
+      "config/test/region".sopsFile = awsSopsFile;
+      "config/qa02/account_id".sopsFile = awsSopsFile;
+      "config/qa02/role_name".sopsFile = awsSopsFile;
+      "config/qa02/region".sopsFile = awsSopsFile;
+      "config/dev02/account_id".sopsFile = awsSopsFile;
+      "config/dev02/role_name".sopsFile = awsSopsFile;
+      "config/dev02/region".sopsFile = awsSopsFile;
+      "config/sandbox/account_id".sopsFile = awsSopsFile;
+      "config/sandbox/role_name".sopsFile = awsSopsFile;
+      "config/sandbox/region".sopsFile = awsSopsFile;
+      "config/produk/account_id".sopsFile = awsSopsFile;
+      "config/produk/role_name".sopsFile = awsSopsFile;
+      "config/produk/region".sopsFile = awsSopsFile;
+      # SSM tunnel secrets
+      "ssm/produk/uk/host".sopsFile = awsSopsFile;
+      "ssm/produk/uk/port".sopsFile = awsSopsFile;
+      "ssm/produk/uk/local_port".sopsFile = awsSopsFile;
     };
 
-    # Generate templates to staging location
     templates."aws/config" = {
       content = ''
 [profile test]
 sso_session = ${config.sops.placeholder."config/sso_session_id"}
 sso_account_id = ${config.sops.placeholder."config/test/account_id"}
-sso_role_name = tech-lead-power-user
-region = ap-southeast-2
+sso_role_name = ${config.sops.placeholder."config/test/role_name"}
+region = ${config.sops.placeholder."config/test/region"}
 
 [profile qa02]
 sso_session = ${config.sops.placeholder."config/sso_session_id"}
 sso_account_id = ${config.sops.placeholder."config/qa02/account_id"}
-sso_role_name = tech-lead-power-user
-region = ap-southeast-2
+sso_role_name = ${config.sops.placeholder."config/qa02/role_name"}
+region = ${config.sops.placeholder."config/qa02/region"}
 
 [profile dev02]
 sso_session = ${config.sops.placeholder."config/sso_session_id"}
 sso_account_id = ${config.sops.placeholder."config/dev02/account_id"}
-sso_role_name = tech-lead-power-user
-region = ap-southeast-2
+sso_role_name = ${config.sops.placeholder."config/dev02/role_name"}
+region = ${config.sops.placeholder."config/dev02/region"}
 
 [profile sandbox]
 sso_session = ${config.sops.placeholder."config/sso_session_id"}
 sso_account_id = ${config.sops.placeholder."config/sandbox/account_id"}
-sso_role_name = tech-lead-admin
-region = ap-southeast-2
+sso_role_name = ${config.sops.placeholder."config/sandbox/role_name"}
+region = ${config.sops.placeholder."config/sandbox/region"}
 
 [profile produk]
 sso_session = ${config.sops.placeholder."config/sso_session_id"}
 sso_account_id = ${config.sops.placeholder."config/produk/account_id"}
-sso_role_name = tech-lead-admin
-region = eu-west-2
+sso_role_name = ${config.sops.placeholder."config/produk/role_name"}
+region = ${config.sops.placeholder."config/produk/region"}
 
 [sso-session ${config.sops.placeholder."config/sso_session_id"}]
 sso_start_url = ${config.sops.placeholder."config/sso_start_url"}
-sso_region = ap-southeast-2
-sso_registration_scopes = sso:account:access
+sso_region = ${config.sops.placeholder."config/sso_region"}
+sso_registration_scopes = ${config.sops.placeholder."config/sso_registration_scopes"}
 
 [default]
-region = ap-southeast-2
+region = ${config.sops.placeholder."config/default_region"}
 '';
       path = "${config.home.homeDirectory}/.config/sops/templates/aws/config";
       mode = "0600";
@@ -96,7 +91,6 @@ region = ap-southeast-2
     };
   };
 
-  # Conditionally copy AWS config files only if they don't exist
   home.activation.copyAwsConfig = config.lib.dag.entryAfter ["sops-nix"] ''
     if [ ! -f "${config.home.homeDirectory}/.aws/config" ]; then
       $DRY_RUN_CMD mkdir -p "${config.home.homeDirectory}/.aws"
